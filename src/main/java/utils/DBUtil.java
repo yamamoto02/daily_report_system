06b8b351f1cc73769ad0421a8d95ef206944564d
yamamoto.N2206 <yamamoto.N2206@ESPE201286.hit-inc.jp>
2022-05-09 14:53:48 +0900

    DBUtil作成

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -4,8 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import constants.JpaConst;
+
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME = "message_board";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
@@ -14,7 +15,7 @@ public class DBUtil {
 
     private static EntityManagerFactory getEntityManagerFactory() {
         if(emf == null) {
-            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            emf = Persistence.createEntityManagerFactory(JpaConst.PERSISTENCE_UNIT_NAME);
         }
 
         return emf;
